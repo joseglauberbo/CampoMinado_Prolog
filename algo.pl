862b216383c433008caa3b3413941fa09dab01d2
samarasss <samara.sampaio@ccc.ufcg.edu.br>
2017-08-11 15:02:56 -0300

    exemplo random

--- algo.pl
+++ algo.pl
@@ -4,3 +4,23 @@ totalColunas(C):- C is 9.
 
 /*Predicado para verificar se a posição é válida, ou seja, se está dentro da matriz*/
 posicaoValida(X,Y):- totalLinhas(L), totalColunas(C), X<L, Y<C. 
+
+/*Gerar random*/
+
+linha is random(1, 9, X).
+coluna is random(1, 9, X).
+
+
+
+
+
+
+/*exemplo de gerar random em c++*/
+
+    for(int i = 0; i <= quant_bombas; i++){
+
+        linha = rand() % size;
+        coluna = rand() % size;
+
+        campo_minado[linha][coluna] = -1;
+ 
