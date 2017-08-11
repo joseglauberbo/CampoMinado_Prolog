323d730f5734782f5ec3adb2a134c1b03751a03e
joseglauberbo <jose.oliveira@ccc.ufcg.edu.br>
2017-08-11 14:52:03 -0300

    criando a matriz e fazendo a funcao para verificar se uma posicao eh valida

--- algo.pl
+++ algo.pl
@@ -1,3 +1,6 @@
+/*Predicados que definem que o tabuleiro será 9x9*/
+totalLinhas(L):- L is 9.
+totalColunas(C):- C is 9.
 
-
-numero_aleatorio is use_module(library(random)).
+/*Predicado para verificar se a posição é válida, ou seja, se está dentro da matriz*/
+posicaoValida(X,Y):- totalLinhas(L), totalColunas(C), X<L, Y<C. 
